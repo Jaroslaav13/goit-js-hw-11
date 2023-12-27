07efdc10b60008f14052a968ef240aa7002ff7fb
Yaroslav <110483805+Jaroslaav13@users.noreply.github.com>
2023-12-27 18:23:42 +0200

    save2

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
